e74eba5edf08bc8a540bba7f40194a422dd9b85c
Chesnokov M.A <chesnokov_ma@mail.ru>
2023-09-19 22:28:47 +0500

    resolve task3 seminar 4

--- seminar4/task_3.py
+++ seminar4/task_3.py
@@ -10,10 +10,10 @@
 операцией, даже ошибочной
 ✔ Любое действие выводит сумму денег
 """
-import decimal
+from decimal import Decimal
 
 # Сумма денег в банкомате. начальная = 0
-summ_atm = decimal.Decimal(0)
+summ_atm = Decimal(0)
 # Счетчик операций
 count_oper = 0
 
@@ -26,22 +26,33 @@ MAX_COMMISSION = 600
 PROCENT_COUNT = 3
 # Число операций для начисления процентов
 PROCENT_COUNT_NUM = 3
+# налог на богатство в процентах
+WEALTH_TAX = 10
+# минимальная сумма для налога на богатство в процентах
+MIN_SUMM_WEALTH_TAX = 5_000_000
+
+def deduct_wealth_tax():
+    """вычитать налог на богатство 10% перед каждой операцией, даже ошибочной"""
+    global summ_atm
+    if summ_atm > MIN_SUMM_WEALTH_TAX:
+        print(f"Сумма {summ_atm.quantize(Decimal('1.00'))} больше {MIN_SUMM_WEALTH_TAX}. Вычитание налога на богатство {WEALTH_TAX}%")
+        summ_atm = Decimal(summ_atm) * Decimal((100 - WEALTH_TAX) / 100)
 
 def interest_calculation():
     """После каждой третей операции пополнения или снятия начисляются проценты PROCENT_COUNT"""
     global summ_atm
     if count_oper % 3 == 0:
-        summ_atm = decimal.Decimal(summ_atm) * decimal.Decimal((100 + PROCENT_COUNT)/100)
-        print(f"Номер операции {count_oper}. Начисление процентов за {PROCENT_COUNT_NUM} операции пополнения или снятия {PROCENT_COUNT} %")
+        summ_atm = Decimal(summ_atm) * Decimal((100 + PROCENT_COUNT)/100)
+        print(f"Номер операции {count_oper}. Начисление процентов за {PROCENT_COUNT_NUM} операции пополнения или снятия {PROCENT_COUNT}%")
 
 def print_sum_atm():
     """выводит сумму денег в банкомате"""
-    print(f"Сумма в банкомате: {summ_atm:.2f} у.е.")
+    print(f"Сумма в банкомате: {summ_atm.quantize(Decimal('1.00'))} у.е.")
 def replenish(sum: int) -> bool:
     """Пополнить счет в банкомате"""
     global summ_atm
     global count_oper
-    summ_atm = decimal.Decimal(summ_atm) + decimal.Decimal(sum)
+    summ_atm = Decimal(summ_atm) + Decimal(sum)
     count_oper += 1
     return True
 
@@ -50,7 +61,7 @@ def issue(sum: int) -> bool:
     global summ_atm
     global count_oper
     if summ_atm >= sum:
-        summ_atm = decimal.Decimal(summ_atm) - decimal.Decimal(sum)
+        summ_atm = Decimal(summ_atm) - Decimal(sum)
         count_oper += 1
     else:
         return False
@@ -61,12 +72,12 @@ def go_out():
     exit()
 
 def main():
-    decimal.getcontext().prec = 2
     while True:
         print("Работа с банкоматом.")
         choice = input('Укажите действие (1 - пополнить; 2 - снять; 3 - выход): ')
         match int(choice):
             case 1:
+                deduct_wealth_tax()
                 sum_input = int(input("Введите сумму пополнения, кратную 50 у.е.: "))
                 if sum_input % 50 == 0:
                     if replenish(sum_input):
@@ -77,12 +88,13 @@ def main():
                     print(f"Сумма {sum_input} не кратна 50 у.е., в операции отказано")
                 interest_calculation()
             case 2:
+                deduct_wealth_tax()
                 sum_input = int(input("Введите сумму снятия, кратную 50 у.е.: "))
                 if sum_input % 50 == 0:
                     if issue(sum_input):
                         print(f"Сумма {sum_input} снята")
                     else:
-                        print(f"Сумма {sum_input} не снята")
+                        print(f"Сумма {sum_input} не может быть снята")
                 else:
                     print(f"Сумма {sum_input} не кратна 50 у.е., в операции отказано")
                 interest_calculation()
